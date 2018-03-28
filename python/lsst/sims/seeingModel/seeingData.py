617c0389c3363e7fc11a214e1ec96c721be9217f
Lynne Jones <lynnej@uw.edu>
2018-03-27 17:08:34 -0700

    Commit initial package, with unit tests and example.

---
+++ python/lsst/sims/seeingModel/seeingData.py
@@ -0,0 +1,97 @@
+from __future__ import division
+from builtins import object
+from datetime import datetime
+import os
+import sqlite3
+import numpy as np
+from lsst.utils import getPackageDir
+
+__all__ = ["SeeingData"]
+
+
+"""
+Thoughts: It would be great to remove TimeHandler because:
+ - the seeing data should probably be tied to an actual time, rather than to just the seconds from the
+start of the simulation. This is because there are weather trends in the summer which are different than
+in the winter (per Eric Neilsen, DESC).
+ - having a dependence on timeHandler here is ok, but it makes it hard for unit tests (unless timehandler is
+ moved to some basic package which other opsim packages could then depend on)
+ """
+
+
+class SeeingData(object):
+    """Read the seeing data from disk and return appropriate FWHM_500 value at a given time.
+
+    Parameters
+    ----------
+    time_handler : :class:`.TimeHandler`
+        The instance of the simulation time handler.
+    seeing_db : str or None, opt
+        The name of the seeing database. If None (default), this will use the Seeing.db file
+        in the 'data' directory of this package.
+    """
+    def __init__(self, time_handler, seeing_db=None):
+        self.seeing_db = seeing_db
+        if self.seeing_db is None:
+            self.seeing_db = os.path.join(getPackageDir('sims_seeingModel'), 'data', 'seeing.db')
+        model_time_start = datetime(time_handler.initial_dt.year, 1, 1)
+        self.offset = time_handler.time_since_given_datetime(model_time_start,
+                                                             reverse=True)
+        self.seeing_dates = None
+        self.seeing_values = None
+
+    def fwhm500_at_time(self, delta_time):
+        """Get the FWHM_500 value for the specified time.
+
+        Parameters
+        ----------
+        delta_time : int
+            The time (seconds) from the start of the simulation.
+
+        Returns
+        -------
+        float
+            The FWHM_500(") closest to the specified time.
+        """
+        delta_time += self.offset
+        # Find the date to look for in the time range of the data.
+        # Note that data dates should not necessarily start at zero.
+        date = delta_time % self.time_range + self.min_time
+        idx = np.searchsorted(self.seeing_dates, date)
+        # searchsorted ensures that left < date < right
+        # but we need to know if date is closer to left or to right
+        left = self.seeing_dates[idx - 1]
+        right = self.seeing_dates[idx]
+        if date - left < right - date:
+            idx -= 1
+        return self.seeing_values[idx]
+
+    def read_data(self):
+        """Read the seeing information from disk.
+
+        The default behavior is to use the module stored database. However, an
+        alternate database file can be provided. The alternate database file needs to have a
+        table called *Seeing* with the following columns:
+
+        seeingId
+            int : A unique index for each seeing entry.
+        s_date
+            int : The time (in seconds) from the start of the simulation, for the seeing observation.
+        seeing
+            float : The FWHM of the atmospheric PSF (in arcseconds) at zenith.
+        """
+        with sqlite3.connect(self.seeing_db) as conn:
+            cur = conn.cursor()
+            query = "select s_date, seeing from Seeing order by s_date;"
+            cur.execute(query)
+            results = np.array(cur.fetchall())
+            self.seeing_dates = np.hsplit(results, 2)[0].flatten()
+            self.seeing_values = np.hsplit(results, 2)[1].flatten()
+            cur.close()
+        # Make sure seeing dates are ordered appropriately (monotonically increasing).
+        ordidx = self.seeing_dates.argsort()
+        self.seeing_dates = self.seeing_dates[ordidx]
+        self.seeing_values = self.seeing_values[ordidx]
+        self.min_time = self.seeing_dates[0]
+        self.max_time = self.seeing_dates[-1]
+        self.time_range = self.max_time - self.min_time
---
+++ python/lsst/sims/seeingModel/seeingSim.py
@@ -0,0 +1,96 @@
+from .seeingData import SeeingData
+from .seeingModel import SeeingModel
+
+__all__ = ['SeeingSim']
+
+class SeeingSim(object):
+    """This is a utility class to combine SeeingData and SeeingFWHM for easier use
+    with the scheduler.
+
+    Parameters
+    ----------
+    time_handler : :class:`.TimeHandler`
+        The instance of the simulation time handler.
+    seeing_db : str or None, opt
+        The name of the seeing database. If None (default), this will use the Seeing.db file
+        in the 'data' directory of this package.
+    telescope_seeing : float, opt
+        The contribution to the FWHM at zenith from the telescope, in arcseconds.
+        Default 0.25"
+    optical_design_seeing : float, opt
+        The contribution to the FWHM at zenith from the optical design, in arcseconds.
+        Default 0.08"
+    camera_seeing : float, opt
+        The contribution to the FWHM at zenith from the camera components, in arcseconds.
+        Default 0.30"
+    raw_seeing_wavelength : float, opt
+        The wavelength (in nm) of the provided value of the atmospheric fwhm at zenith.
+        Default 500nm.
+    filters_effwavelen : numpy.ndarray or None, opt
+        An array containing the effective wavelengths per filter in filter_list, in nm.
+        If this is None (default), sims_photUtils will be used to calculate the values for ugrizy
+        based on the setup throughputs repository.
+    """
+    def __init__(self,  time_handler, seeing_db=None, telescope_seeing=0.25,
+                 optical_design_seeing=0.08, camera_seeing=0.30,
+                 raw_seeing_wavelength=500,
+                 filter_effwavelens=None, filter_list=None):
+        # Set up seeing data, including reading from disk.
+        self.seeing_data = SeeingData(time_handler=time_handler, seeing_db=seeing_db)
+        self.seeing_data.read_data()
+        # Set up seeing model, including the filter wavelengths.
+        self.seeing_model = SeeingModel(telescope_seeing=telescope_seeing,
+                                       optical_design_seeing=optical_design_seeing,
+                                       camera_seeing=camera_seeing,
+                                       raw_seeing_wavelength=raw_seeing_wavelength,
+                                       filter_effwavelens=filter_effwavelens)
+        if filter_effwavelens is not None:
+            if filter_list is None:
+                raise ValueError('If filter_effwavelens is specified, so must filter_list.')
+            if len(filter_list) != len(filter_effwavelens):
+                raise ValueError('Length of filter_list and filter_effwavelens must match.')
+            self.seeing_model.filter_list = filter_list
+        self.filter_list = self.seeing_model.filter_list
+
+    def calculate_seeing(self, delta_time, filter_name, airmass):
+        """Calculate seeing in a single filter -- for backwards compatibility only.
+
+        Parameters
+        ----------
+        delta_time : int
+            The time (seconds) from the start of the simulation.
+        filter_name : str
+            The single character filter name for the calculation.
+        airmass : float
+            The airmass for the calculation.
+        Returns
+        -------
+        tuple
+            The FWHM 500nm, FWHM Geometric and FWHM Effective seeing values.
+        """
+        fwhm_500 = self.seeing_data.fwhm500_at_time(delta_time)
+        fwhm_eff, fwhm_geom = self.seeing_model.seeing_at_airmass(fwhm_500, airmass=airmass)
+        idx = self.filter_list.index(filter_name)
+        fwhm_eff = fwhm_eff[idx]
+        fwhm_geom = fwhm_geom[idx]
+        return (fwhm_500, fwhm_geom, fwhm_eff)
+
+    def get_seeing(self, delta_time, airmass):
+        """Calculate seeing in all filters.
+
+        Parameters
+        ----------
+        delta_time : int
+            The time (seconds) from the start of the simulation.
+        airmass : float or np.ndarray
+            The airmass value(s) at which to calculate seeing.
+
+        Returns
+        -------
+        tuple
+            The FWHM 500nm, FWHM Geometric and FWHM Effective seeing values,
+            where FWHM_geom and FWHM_eff are np.ndarrays (1-d if airmass is float, 2-d if array).
+        """
+        fwhm_500 = self.seeing_data.fwhm500_at_time(delta_time)
+        fwhm_eff, fwhm_geom = self.seeing_model.seeing_at_airmass(fwhm_500, airmass=airmass)
+        return (fwhm_500, fwhm_geom, fwhm_eff)
